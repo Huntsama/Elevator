da1e50006e9f8a661002cdb10b525cfb84ae4c6a
Huntsama <jawad.itoube@gmail.com>
2022-06-21 04:56:39 +0100

    coding session

--- src/App.js
+++ src/App.js
@@ -1,23 +1,43 @@
-import logo from './logo.svg';
+import React, { useState, useEffect } from 'react';
 import './App.css';
 
 function App() {
+
+  const [count, setCount] = useState(0);
+
+  const increment = () => {
+    setCount(count + 1);
+  };
+  const decrement = () => {
+    setCount(count - 1);
+  };
+
+  useEffect(() => {
+
+    document.title = ` the count is${count}`;
+  });
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="items-center text-center mt-80 ">
+     
+      <div className=" flex-row "> 
+      <h1 className=" text-3xl font-bold ">
+        {count}
+      </h1>
+
+
+
+      <button onClick={decrement} className="bg-red-500 hover:bg-red-700 text-white font-bold py-2 px-4 rounded mt-5 mr-2">
+        -1
+      </button>
+
+      <button onClick={increment} className="bg-red-500 hover:bg-red-700 text-white font-bold py-2 px-4 rounded mt-5 ml-2 ">
+        +1
+      </button>
+
+
+      </div>
+
     </div>
   );
 }
